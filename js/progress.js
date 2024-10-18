7520b0bf40fd5a0b78e67132d400a948c7ae5042
Tsiprovskiy Egor <egor-ciprov@mail.ru>
2024-10-18 16:36:35 +0300

    add jsdoc class Progress

--- js/progress.js
+++ js/progress.js
@@ -1,3 +1,14 @@
+/**
+ * Класс для создания progress блока
+ * @class
+ * @param {string} idSvg - id svg блока
+ * @param {string} idCircle1 - id первого круга
+ * @param {string} idCircle2 - id второго круга
+ * @param {string} color1 - цвет первого круга
+ * @param {string} color2 - цвет второго круга
+ * @param {number} [value=0] - значение
+ * @param {string} [state="normal"] - состояние progress блока (normal, animated, hidden)
+ */
 export class Progress {
   constructor(
     idSvg,
@@ -20,6 +31,10 @@ export class Progress {
     this.render();
     this.initialState(state);
   }
+  /**
+   * Создает svg блок с двумя кругами
+   * @returns {Element} - svg блок
+   */
   createCircleBlock() {
     const svgNS = "http://www.w3.org/2000/svg";
     const svgContainer = document.createElementNS(
@@ -58,9 +73,16 @@ export class Progress {
 
     return svgContainer;
   }
+  /**
+   * Рендерит svg блок
+   */
   render() {
     document.getElementById("circle-container").append(this.svgElement);
   }
+  /**
+   * Устанавливает начальное состояние progress блока
+   * @param {string} state - состояние progress блока (normal, animated, hidden)
+   */
   initialState(state) {
     if (state == "normal") {
       this.setValue(this.value);
@@ -72,15 +94,23 @@ export class Progress {
       this.setHidden(true);
     }
   }
+  /**
+   * Устанавливает значение progress блока
+   * @param {number} value - значение
+   */
   setValue(value) {
     const circle = document.getElementById(this.idCircle2);
     const offset = 628 - (628 * value) / 100;
     circle.style.strokeDashoffset = offset + "px";
   }
+  /**
+   * Устанавливает анимацию progress блока
+   * @param {boolean} isAnimated - флаг анимации
+   */
   setAnimated(isAnimated) {
     let val = 0;
     const self = this;
-    const circle = document.getElementById(self.idCircle2);
+    const circle = document.getElementById(this.idCircle2);
 
     function startProgress() {
       self.timerId = setInterval(() => {
@@ -107,6 +137,10 @@ export class Progress {
       stopProgress();
     }
   }
+  /**
+   * Устанавливает скрытие progress блока
+   * @param {boolean} isHidden - флаг скрытия
+   */
   setHidden(isHidden) {
     if (isHidden) {
       this.svgElement.classList.add("hidden");
